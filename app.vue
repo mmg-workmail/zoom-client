717b12b3bcc8677bc961470821ae139057a4da39
mohammad reza <mmg.workmail@gmail.com>
2024-04-22 12:38:44 +0300

    add meeting sdk

--- app.vue
+++ app.vue
@@ -1,78 +1,109 @@
 <template>
   <div>
-    <button @click="joinGame">
-      Meeting
-    </button>
+    <div class="">
+      <button @click="createMeeting">Create Meeting</button>
+    </div>
+    <input v-model="paramsToken.username" />
+    <select v-model="paramsToken.side">
+      <option value="mafia">Mafia</option>
+      <option value="civil">Civil</option>
+    </select>
+    <button @click="joinGame">Join to meeting</button>
     <pre>
       {{ tokenData?.data }}
     </pre>
+    <div id="meetingSDKElement">
+      <!-- Meeting SDK renders here when a user starts or joins a Zoom meeting -->
+    </div>
   </div>
 </template>
 
-
 <script setup lang="ts">
-import ZoomVideo from '@zoom/videosdk'
+import ZoomMtgEmbedded from "@zoom/meetingsdk/embedded";
 
 interface CreateMeeting {
   data: {
     meeting: {
-      id: number,
-      password: string
-    }
-  }
+      id: number;
+      password: string;
+    };
+  };
 }
 
-interface  JoinMeeting {
-  data:{
-    clientId: string,
-    meetingId: number,
-    password: string,
-    token: string,
-  }
+interface JoinMeeting {
+  data: {
+    clientId: string;
+    meetingId: number;
+    password: string;
+    token: string;
+  };
 }
 
-const {data: meetingData, execute: meetingExecute, error: meetingError} = await useFetch<CreateMeeting>(
-  'http://mafia-test.caspadm.com/api/v1/game/meeting',
+const {
+  data: meetingData,
+  execute: meetingExecute,
+  error: meetingError,
+} = await useFetch<CreateMeeting>(
+  "http://mafia-test.caspadm.com/api/v1/game/meeting",
   {
     headers: {
-      Authorization : 'Basic '+btoa(`mafia:ae7404e5a091788fd829bb04463f9832`)
+      Authorization: "Basic " + btoa(`mafia:ae7404e5a091788fd829bb04463f9832`),
     },
-    method: 'post',
-    immediate: false
+    method: "post",
+    immediate: false,
   }
-)
-
+);
 
 const paramsToken = reactive({
-    "meetingId": meetingData.value?.data.meeting.id,
-    "username": "mohammadreza",
-    "side": "mafia"
-})
-const { data: tokenData, execute: tokenExecute, error: tokenError } = await useFetch<JoinMeeting>('http://mafia-test.caspadm.com/api/v1/game/generateToken', {
-  method: 'post',
-  body: paramsToken,
-  immediate: false
-})
-
-async function joinGame() {
-  await meetingExecute()
-  if (meetingData.value) {
-    paramsToken.meetingId = meetingData.value.data.meeting.id
+  meetingId: meetingData.value?.data.meeting.id,
+  username: "mohammadreza",
+  side: "mafia",
+});
+const {
+  data: tokenData,
+  execute: tokenExecute,
+  error: tokenError,
+} = await useFetch<JoinMeeting>(
+  "http://mafia-test.caspadm.com/api/v1/game/generateToken",
+  {
+    method: "post",
+    body: paramsToken,
+    immediate: false,
+    watch: false,
   }
-  //await tokenExecute()
-  //await createZoomVideo();
+);
+
+async function createMeeting() {
+  await meetingExecute();
+  if (meetingData.value) {
+    paramsToken.meetingId = meetingData.value.data.meeting.id;
+  }
+}
+async function joinGame() {
+  await tokenExecute();
+  joinPerson();
 }
 
+const client = ref(ZoomMtgEmbedded.createClient());
 
-async function createZoomVideo () {
-  console.log('testGame', tokenData.value?.data.token, paramsToken.username)
-  var client = ZoomVideo.createClient()
-  var stream
+async function createZoomVideo() {
+  let meetingSDKElement = document.getElementById("meetingSDKElement");
 
-  await client.init('en-US', 'Global', { patchJsMedia: true })
-  await client.join('testGame', tokenData.value?.data.token, paramsToken.username)
-
-  stream = client.getMediaStream()
+  client.value.init({ zoomAppRoot: meetingSDKElement, language: "en-US" });
 }
 
+async function joinPerson() {
+  client.value.join({
+    sdkKey: tokenData.value?.data.clientId,
+    signature: tokenData.value?.data.token as string, // role in SDK signature needs to be 1
+    meetingNumber: tokenData.value?.data.meetingId?.toString() as string,
+    password: tokenData.value?.data.password,
+    userName: paramsToken.username,
+    // zak: "", // the host's zak token
+  });
+}
+
+onMounted(() => {
+  createZoomVideo();
+});
 </script>
